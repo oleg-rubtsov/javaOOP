144f76e9d3c1c7fd78470da10ea0fa5cc6ec7221
Oleg Rubtsov <oleg.rubtsov99@gmail.com>
2023-07-07 10:40:15 +0300

    trying to use package

---
+++ src/tracker/Training.java
@@ -0,0 +1,11 @@
+package tracker;
+
+public class Training {
+    public static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        // myMethod();
+    }
+}
