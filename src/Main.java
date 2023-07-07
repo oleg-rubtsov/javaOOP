1562e74eb26c40a664d12dc23f51055525f2cd91
Oleg Rubtsov <oleg.rubtsov99@gmail.com>
2023-07-07 12:18:07 +0300

    learn construct

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        Training obj = new Training();
+        Training obj = new Training(1, 2);
         obj.myMethod();
+        System.out.println(obj.get_distance());
     }
 }
--- src/tracker/Training.java
+++ src/tracker/Training.java
@@ -1,11 +1,21 @@
 package tracker;
 
 public class Training {
+    public int a;
+    public int b;
+    public Training(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
     public static void myMethod() {
         System.out.println("Hello World!");
     }
 
-    public static void main(String[] args) {
-        // myMethod();
+    public String get_distance() {
+        return ("Values^=:" + this.a + "/" + this.b);
     }
+//
+//    public static void main(String[] args) {
+//        // myMethod();
+//    }
 }
